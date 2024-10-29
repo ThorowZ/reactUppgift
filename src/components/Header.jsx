ea1cdf4a179b22f9bc7b0ea0c40a25ddd5a74d0e
ThorowZ <dennisgranatdennisgranat@hotmail.com>
2024-10-29 15:29:50 +0100

    React första push

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <section className="Header">
+      <div className="Header-content">
+        <div className="text-card">
+          <div className="header-card">
+            <h1 className="header1" aria-label="Manage money">Manage All Your Money in One App</h1>
+          </div>
+          <p className="subheader">
+            We offer you a new generation of mobile banking. Save, spend & manage money in your pocket.
+          </p>
+          <div className="market-btns">
+            <img src="/Images/Apple.svg" alt="Apple Store" />
+            <img src="/Images/Google.svg" alt="Google Store" />
+          </div>
+          <div className="more">
+            <img src="/Images/more.svg" alt="Discover More" />
+          </div>
+        </div>
+        <div className="iphone-container">
+          <div className="iphone12">
+            <img src="Images/iPhone12Pro.svg" alt="iPhone 12 Pro" />
+            <img src="Images/iPhone12Pro(1).svg" alt="iPhone 12 Pro" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar-background">
+      <div className="navbar">
+        {/* Silicon logo */}
+        <a href="#">
+          <img id="solid" src="Images/solid.svg" alt="Silicon Logo" />
+        </a>
+
+        {/* Feature link */}
+        <div id="Feature">
+          <a href="#" aria-current="page" tabIndex="0">Feature</a>
+        </div>
+
+        {/* Dark mode and bright mode switch */}
+        <p className="theme-text"></p>
+        <label className="toggle" htmlFor="darkmode-switch" aria-label="darkmode-switch">
+          <input type="checkbox" id="darkmode-switch" />
+          <span className="slider"></span>
+        </label>
+
+        {/* Sign in button */}
+        <button id="sign-in-btn">
+          <img className="icon-l" src="Images/icon-l.svg" alt="Icon" />
+          Sign in/up
+        </button>
+
+        {/* Mobile menu button */}
+        <img id="Button-Mobil-bright" src="Images/Button-Mobil-bright.svg" alt="Mobile tab bar" />
+      </div>
+    </nav>
+  );
+}
